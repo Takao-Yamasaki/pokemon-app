adf9c74be964742c97576f28b33ea64eefc5789b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-09-17 21:49:18 +0900

    add:ローディング設定

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  // 最初からポケモンのデータを取得するため、loadingする前提
+  const [loading, setLoading] = useState(true);
 
   // 一回だけ呼び出したいので、第二引数は[]で指定する
   useEffect(() => {
@@ -12,12 +14,22 @@ function App() {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      // データを取得できたので、ローディングはしない
+      setLoading(false);
     };
     // fetchPokemonDataを呼び出す
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ) :(
+      <>
+        <h1>ポケモンデータを取得しました</h1>
+      </>
+    )}
+  </div>;
 }
 
 export default App;
